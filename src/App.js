3829954182461a71a6382bbadf40f1f97f541bcd
chedi <chediouerghi88@gmail.com>
2023-08-13 23:35:54 +0100

    update 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import Roulette from './components/page/Roulette';
-import Home from './components/home/Home';
 
 function App() {
-  const [showRoulette, setShowRoulette] = useState(false);
 
-  const startRoulette = () => {
-    setShowRoulette(true);
-  };
 
   return (
     <div className="App">
-      {showRoulette ? (
         <Roulette />
-      ) : (
-        <Home startRoulette={startRoulette} />
-      )}
     </div>
   );
 }
